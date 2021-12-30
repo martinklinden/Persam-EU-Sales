440c114e8983676df26aab3ff17b42a71a70bdf3
martinklinden <72434888+martinklinden@users.noreply.github.com>
2021-12-30 19:05:06 +0100

    +Changes in tsconfig +New methods to handle csv-content, but only accessible inside onload !need to use services instead

--- src/app/csv-import/csv-import.component.ts
+++ src/app/csv-import/csv-import.component.ts
@@ -7,27 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CsvImportComponent implements OnInit {
 
-  public fileContent: string;
-  public file: any;
+  public csvContent: string;
+  public parsedCsv: string[][];
 
   constructor() { 
-    this.fileContent = "";
   }
 
   ngOnInit(): void {
   }
 
-  fileChanged(e:any) {
-    this.file = e.target.files[0];
-    this.uploadDocument();
+  onFileLoad(fileLoadedEvent: any): void {
+    const csvSeparator = ';';
+    const textFromFileLoaded: string = fileLoadedEvent.target.result;              
+    this.csvContent = textFromFileLoaded;     
+    
+    // parsing the csv
+    const txt = textFromFileLoaded;
+    const csv:any = [];
+    const lines = txt.split('\n');
+    lines.forEach(element => {
+      const cols: string[] = element.split(csvSeparator);
+      csv.push(cols);
+      });
+    this.parsedCsv = csv;
+    
+    // // test
+    // console.log(this.parsedCsv);
+    // console.log(this.parsedCsv[0]);
+    // console.log(this.parsedCsv[0][0]);
+    // alert(this.parsedCsv[0][0]);
+    
+    // // demo output as alert
+    // var output: string="";
+    // csv.forEach(row => {
+    //   output += "\n";
+    //   var colNo = 0;
+    //   row.forEach(col => {
+    //     if (colNo>0) output += " | ";
+    //     output += col;
+    //     colNo++;
+    //   });
+    // });
+    // alert(output);
   }
 
-  uploadDocument() {
-    let fileReader = new FileReader();
-    fileReader.onload = (e) => {
-      // console.log(fileReader.result);
-      this.fileContent = fileReader.result as string
+  onFileSelect(input: any) {
+
+    const files = input.files;
+    // var content = this.csvContent; ???
+    
+    if (files && files.length) {
+       
+        // console.log("Filename: " + files[0].name);
+        // console.log("Type: " + files[0].type);
+        // console.log("Size: " + files[0].size + " bytes");        
+
+      const fileToRead = files[0];
+      const fileReader = new FileReader();
+
+      fileReader.onload = this.onFileLoad; 
+      fileReader.readAsText(fileToRead, "UTF-8");
     }
-    fileReader.readAsText(this.file);    
   }
 }
