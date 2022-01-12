968a92068efa21e6ba42b864266c0824baf84545
martinklinden <72434888+martinklinden@users.noreply.github.com>
2022-01-12 16:52:00 +0100

    The csv-file can now contain data from textboxes +some html fixes ! the current export only works for the testfile, due to it removing the first row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,17 @@ import { saveAs } from 'file-saver'
 })
 export class AppComponent {
   title = 'persam-eu-sales';
-  fileName: string = "TEMP";
+  orgNr: string = "";
+  period: string = "";
+  name: string = "";
+  phone: string = "";
+  email: string = "";
+  fileName: string = "csvfilnamn";
+  csvCode: string = "SKV574008";
   csvColumns: string[][];
   exportColumns: string[][];
 
-  receiveCsvImportEvent($event){
+  receiveCsvImportEvent($event) {
     this.csvColumns = $event;
     // hide elements?
     // make nulllable check instead?
@@ -20,27 +26,41 @@ export class AppComponent {
     // document.getElementById("columns")!.style.display = "block";
   }
 
-  receiveColumnDataEvent($event){
+  receiveColumnDataEvent($event) {
     this.exportColumns = $event;
   }
-  
-  saveFile(){
-    this.fileName = (<HTMLInputElement>document.getElementById('fileName')).value;
-    const blob = new Blob([this.createExportString(this.rearrangeCsv())], 
-    {type: "text/csv; charset=UTF-8"});
-    saveAs(blob, `${ this.fileName }.csv`);
-  }
-  
-  createExportString(exportArray: string[][]){
-    let exportString:string = "";
 
-    for(let i = 0; i < exportArray.length; i++){
-      for(let j = 0; j < exportArray[i].length; j++){
-        if(j == exportArray[i].length - 1){
+  saveFile() {
+    this.fileName = (<HTMLInputElement>document.getElementById('fileName')).value;
+    this.orgNr = (<HTMLInputElement>document.getElementById('orgNr')).value;
+    this.period = (<HTMLInputElement>document.getElementById('period')).value;
+    this.name = (<HTMLInputElement>document.getElementById('name')).value;
+    this.phone = (<HTMLInputElement>document.getElementById('phone')).value;
+    this.email = (<HTMLInputElement>document.getElementById('email')).value;
+    const blob = new Blob([this.createExportString(this.rearrangeCsv())],
+      { type: "text/csv; charset=UTF-8" });
+    saveAs(blob, `${this.fileName}.csv`);
+  }
+
+  createExportString(exportArray: string[][]) {
+    let exportString: string = "";
+
+    exportString += `${this.csvCode};;;;\n`; //several separators not needed?
+    exportString += `${this.orgNr};`;
+    exportString += `${this.period};`;
+    exportString += `${this.name};`;
+    exportString += `${this.phone};`;
+    exportString += `${this.email}\n`;
+
+    // cheating by setting i to 1 and skipping the first row, fix when editing of column is added
+    for (let i = 1; i < exportArray.length; i++) {
+      for (let j = 0; j < exportArray[i].length; j++) {
+
+        if (j == exportArray[i].length - 1) {
           exportString += exportArray[i][j]; //doesn't contain separator
         }
-        else{
-          exportString += `${ exportArray[i][j] };`; //contains separator
+        else {
+          exportString += `${exportArray[i][j]};`; //contains separator
         }
       }
       //exportString += "\n"; // maybe not needed, depends on importfile?
@@ -49,15 +69,15 @@ export class AppComponent {
   }
 
   //same as createColumns in csv-import, create a method/class that both components can use??
-  rearrangeCsv(){
+  rearrangeCsv() {
     let exportArray: string[][] = new Array<Array<string>>();
 
-    for(let i = 0; i < this.exportColumns[0].length; i++){
+    for (let i = 0; i < this.exportColumns[0].length; i++) {
       exportArray[i] = [];
     }
 
-    for(let i = 0; i < this.exportColumns.length; i++){
-      for(let j = 0; j < this.exportColumns[i].length; j++){
+    for (let i = 0; i < this.exportColumns.length; i++) {
+      for (let j = 0; j < this.exportColumns[i].length; j++) {
         exportArray[j].push(this.exportColumns[i][j]);
       }
     }
